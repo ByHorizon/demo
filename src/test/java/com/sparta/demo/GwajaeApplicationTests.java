3c8030259974884a5415b40cc890df629de7896d
ByHorizon <rptedpa5@naver.com>
2023-06-26 10:40:49 +0900

    Initial commit

---
+++ src/test/java/com/sparta/demo/GwajaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GwajaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
